832db10b458bd3ef435ca512de3a91eb1cdbcafe
Moonitime: Ultra Romantic <moonitimeultraromantic@gmail.com>
2022-07-30 21:16:12 +0000

    lavender: Project Kasumi Bringup
    [windowz414: Adjust dependencies.]
    [windowz414: Utilize CUSTOM_VENDOR_DIR.]
    [windowz414: Include GCGOP.]
    [windowz414: Translate Mooni's name to Japanese (values-jp).]
    
    Co-authored-by: Beru Hinode <windowz414@1337.lgbt>
    Signed-off-by: Moonitime: Ultra Romantic <moonitimeultraromantic@gmail.com>
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_lavender.mk
+    $(LOCAL_DIR)/kasumi_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_lavender-user \
-    arrow_lavender-userdebug \
-    arrow_lavender-eng
+    kasumi_lavender-user \
+    kasumi_lavender-userdebug \
+    kasumi_lavender-eng
